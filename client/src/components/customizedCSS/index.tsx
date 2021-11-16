e6ab7312d26a18561c999e5b7203ec2b63e0b06f
Andrea <ngocanhhsgs@gmail.com>
2021-11-16 15:09:07 +0200

    add holiday theme

--- client/src/components/customizedCSS/index.tsx
+++ client/src/components/customizedCSS/index.tsx
@@ -43,39 +43,10 @@ export const BoxRow = styled(Box)`
   align-items: center;
 `
 
-export const CustomizedButton = styled(Button)`
-  color: white;
-  border-radius: 20px;
-  background-color: #16213e;
-  padding: 10px;
-  border: 1px white solid;
-  margin: 20px;
-  width: 150px;
-  font-family: 'Open Sans Condensed', sans-serif;
-`
-
 export const CustomizedLPlayIcon = styled(PlayCircleOutlineIcon)`
   color: white;
   width: 50px;
   height: 50px;
-`
-
-//HOLIDAY THEME
-export const CustomizedTitle = styled(Typography)`
-  text-align: center;
-  margin: 10px;
-  font-family: 'Open Sans Condensed', sans-serif;
-  font-size: 2rem;
-  background-image: linear-gradient(
-    92deg,
-    #a48b5e -31%,
-    #ce944a -7%,
-    #d8a965 11%,
-    #f9f3e2 121%,
-    #f4e3af 132%
-  );
-  background-clip: text;
-  text-fill-color: transparent;
 `
 
 export const CustomizedText = styled(Typography)`
@@ -101,4 +72,32 @@ export const CustomizedTextProduct = styled(Typography)`
   display: -webkit-box;
   -webkit-box-orient: vertical;
   overflow: hidden;
+`
+
+//HOLIDAY THEME
+export const CustomizedTitle = styled(Typography)`
+  text-align: center;
+  margin: 10px;
+  font-family: 'Open Sans Condensed', sans-serif;
+  font-size: 2rem;
+  background-image: linear-gradient(
+    92deg,
+    #a48b5e -31%,
+    #ce944a -7%,
+    #d8a965 11%,
+    #f9f3e2 121%,
+    #f4e3af 132%
+  );
+  background-clip: text;
+  text-fill-color: transparent;
+`
+export const CustomizedButton = styled(Button)`
+  color: white;
+  border-radius: 20px;
+  background-color: #16213e;
+  padding: 10px;
+  border: 1px white solid;
+  margin: 20px;
+  width: 150px;
+  font-family: 'Open Sans Condensed', sans-serif;
 `

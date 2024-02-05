ff1dd4e42c2c8d452cecfdec7e83969309946ebc
andrea <ngocanhhsgs@gmail.com>
2024-02-05 22:48:04 +0200

    Revert "Extract seed product into script"
    This reverts commit 2b0149a73dff7aab4058c5454e83892bc1c629da.

--- src/routers/product.ts
+++ src/routers/product.ts
@@ -6,13 +6,15 @@ import {
   deleteProduct,
   findAll,
   updateProduct,
+  seedProduct,
 } from '../controllers/product'
 
 const router = express.Router()
 
-// Every path we define here will get /api/v1/product prefix
+// Every path we define here will get /api/v1/movies prefix
 router.post('/', createProduct)
 router.put('/:productId', updateProduct)
+// router.get('/seed', seedProduct)
 router.get('/', findAll)
 router.get('/:productId', findById)
 router.delete('/:productId', deleteProduct)
